e56359d4ee86fa360ee5df5eb9a9376d5b004267
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-18 22:06:06 +0900

    Feat: Member 도메인 생성

---
+++ src/main/java/goormthon/team28/startup_valley/domain/Member.java
@@ -0,0 +1,35 @@
+package goormthon.team28.startup_valley.domain;
+
+import goormthon.team28.startup_valley.dto.type.EPart;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@DynamicUpdate
+@Table(name = "members")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+    @Column(name = "part")
+    @Enumerated(EnumType.STRING)
+    private EPart part;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(name = "total_minute")
+    private Long totalMinute;
+    @Column(name = "retrospection")
+    private String retrospection;
+
+
+}
