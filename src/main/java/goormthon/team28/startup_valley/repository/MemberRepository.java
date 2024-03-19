991ef8f9115e22fcb892110bed1158eb72ca1d56
JeongHeumChoi <andyc707@gmail.com>
2024-03-20 01:48:13 +0900

    feat: 팀 작업 척도 서비스 구현

--- src/main/java/goormthon/team28/startup_valley/repository/MemberRepository.java
+++ src/main/java/goormthon/team28/startup_valley/repository/MemberRepository.java
@@ -12,6 +12,8 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByIdAndUser(Long teamId, User user);
-    Boolean existsByUserId(Long userId);
-    List<Member> findByTeam(Team team);
+    Boolean existsByUser(User user);
+    Boolean existsByUserAndTeam(User user, Team team);
+    List<Member> findAllByTeam(Team team);
+    List<Member> findAllByTeamOrderByTotalMinuteDesc(Team team);
 }
