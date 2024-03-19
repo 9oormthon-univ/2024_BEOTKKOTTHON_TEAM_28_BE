427f6e9cc5c458f73ba2e38bfb0cd31a7422584f
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-20 01:33:01 +0900

    ✨ [Feature/#7] - discord slash command, 질문하기 (#52)
    * Chore: discord token 추가
    
    * Feat: Question column 추가
    
    * Feat: code 관련 조회 로직 추가
    
    * Chore: 서비스 단 조회 로직 추가
    
    * Feat: generate code 로직 생성
    
    * Feat: 질문하기 slash command 추가
    
    * Feat: slash command action 추가

--- src/main/java/goormthon/team28/startup_valley/discord/listener/DiscordListener.java
+++ src/main/java/goormthon/team28/startup_valley/discord/listener/DiscordListener.java
@@ -1,22 +1,26 @@
 package goormthon.team28.startup_valley.discord.listener;
 
+import goormthon.team28.startup_valley.domain.Question;
 import goormthon.team28.startup_valley.domain.Team;
 import goormthon.team28.startup_valley.dto.type.EProjectStatus;
 import goormthon.team28.startup_valley.repository.MemberRepository;
 import goormthon.team28.startup_valley.repository.TeamRepository;
 import goormthon.team28.startup_valley.repository.UserRepository;
 import goormthon.team28.startup_valley.service.MemberService;
+import goormthon.team28.startup_valley.service.QuestionService;
 import goormthon.team28.startup_valley.service.TeamService;
 import goormthon.team28.startup_valley.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.transaction.annotation.Transactional;
 
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,12 +30,14 @@ public class DiscordListener extends ListenerAdapter {
     private final UserService userService;
     private final TeamService teamService;
     private final MemberService memberService;
+    private final QuestionService questionService;
     @Override
     @Transactional
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        LocalDate nowLocalDate = LocalDate.now();
+        LocalDateTime nowLocalDateTime = LocalDateTime.now();
         switch (event.getName()) {
-            case "웹과연동하기":
-                LocalDate now = LocalDate.now();
+            case "팀원업데이트":
                 // 봇을 제외한 사용자 목록 생성
                 List<Member> discordMembers = event.getGuild().getMembers().stream()
                         .filter(member -> !member.getUser().isBot()).toList();
@@ -40,7 +46,7 @@ public class DiscordListener extends ListenerAdapter {
                 List<String> noSignUp = findNoSignUp(discordMembers);
                 if (noSignUp.isEmpty()){ // 모두 회원가입을 한 경우
                     // 팀을 생성하거나 조회한다
-                    Team team = teamService.saveTeam(event.getGuild().getId(), event.getGuild().getName(), event.getGuild().getIconUrl(), now);
+                    Team team = teamService.saveTeam(event.getGuild().getId(), event.getGuild().getName(), event.getGuild().getIconUrl(), nowLocalDate);
                     log.info("팀 생성 완료");
 
                     // 팀의 멤버를 생성하거나 조회한다.
@@ -60,6 +66,24 @@ public class DiscordListener extends ListenerAdapter {
                 } else {
                     event.reply("웹에 회원가입이 필요합니다!\n\n" + "회원가입 해주세요 !! : " + noSignUp.toString()).setEphemeral(true).queue();
                 }
+                break;
+            case "질문하기":
+                String guildId = event.getGuild().getId();
+                String senderId = event.getMember().getUser().getName();
+                User receiver = event.getOption("receiver").getAsUser();
+                String receiverId = receiver.getName();
+                String questionContent = event.getOption("question_content").getAsString();
+                // 질문 생성
+                Question question = questionService.saveQuestion(guildId, senderId, receiverId, questionContent, nowLocalDateTime);
+
+                // 질문 생성 완료, 질문자에게 알리는 내용
+                event.reply("질문이 등록 되었습니다 ! ").setEphemeral(true).queue();
+                // 질문 생성 완료, 답변자 및 팀원에게 알리는 내용
+                event.getGuild().getTextChannelById(event.getChannel().getId())
+                        .sendMessage(receiver.getAsMention() + "님! 답변을 기다리는 질문이 생성되었어요 ! \n\n" +
+                                "디스코드를 통해 답변하는 경우에는 이 코드를 사용해주세요 ! code: "+ question.getCode())
+                        .queue();
+
                 break;
         }
     }
