26025b24abcc86e1265fc0dd4fd3d9c6fb305d5e
JeongHeumChoi <andyc707@gmail.com>
2024-03-20 01:44:34 +0900

    fix: 멤버가 존재하는지 예외처리

--- src/main/java/goormthon/team28/startup_valley/service/MemberService.java
+++ src/main/java/goormthon/team28/startup_valley/service/MemberService.java
@@ -9,6 +9,7 @@ import goormthon.team28.startup_valley.exception.CommonException;
 import goormthon.team28.startup_valley.exception.ErrorCode;
 import goormthon.team28.startup_valley.repository.MemberRepository;
 import goormthon.team28.startup_valley.repository.TeamRepository;
+import goormthon.team28.startup_valley.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,15 +24,19 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final TeamRepository teamRepository;
+    private final UserRepository userRepository;
 
     public MemberListDto listTeamMember(Long userId, Long teamsId) {
 
-        if (memberRepository.existsByUserId(userId))
+        User currentUser = userRepository.findById(userId)
+                .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_USER));
+
+        if (memberRepository.existsByUser(currentUser))
             throw new CommonException(ErrorCode.NOT_FOUND_MEMBER);
 
         Team team = teamRepository.findById(teamsId)
                 .orElseThrow(() -> new CommonException(ErrorCode.NOT_FOUND_TEAM));
-        List<Member> memberList = memberRepository.findByTeam(team);
+        List<Member> memberList = memberRepository.findAllByTeam(team);
         List<MemberDto> memberDtoList = memberList.stream()
                 .map(member -> MemberDto.of(
                         member.getId(),
