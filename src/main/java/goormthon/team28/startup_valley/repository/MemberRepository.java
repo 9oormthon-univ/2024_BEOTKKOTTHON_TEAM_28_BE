769158347d54131ee5696498a958cbb204177e36
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-20 18:06:55 +0900

    Feat: 누적 업무 시간, 업무 종료시 데이터 업데이트 로직 추가

--- src/main/java/goormthon/team28/startup_valley/repository/MemberRepository.java
+++ src/main/java/goormthon/team28/startup_valley/repository/MemberRepository.java
@@ -4,6 +4,8 @@ import goormthon.team28.startup_valley.domain.Member;
 import goormthon.team28.startup_valley.domain.Team;
 import goormthon.team28.startup_valley.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,4 +20,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findAllByTeamOrderByTotalMinuteDesc(Team team);
     List<Member> findAllByUser(User user);
     Optional<Member> findByTeamAndUser(Team team, User user);
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.totalMinute = :totalMinute where m.id = :memberId")
+    void updateTotalMinute(Long memberId, Long totalMinute);
 }
--- src/main/java/goormthon/team28/startup_valley/repository/WorkRepository.java
+++ src/main/java/goormthon/team28/startup_valley/repository/WorkRepository.java
@@ -4,8 +4,11 @@ import goormthon.team28.startup_valley.domain.Member;
 import goormthon.team28.startup_valley.domain.Scrum;
 import goormthon.team28.startup_valley.domain.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +16,8 @@ import java.util.Optional;
 public interface WorkRepository extends JpaRepository<Work, Long> {
     Optional<Work> findByScrumAndOwnerAndEndAtIsNull(Scrum scrum,Member owner);
     List<Work> findAllByOwner(Member member);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update Work w set w.content = :content, w.endAt = :now where w.id = :workId")
+    void updateWorkAfterOver(Long workId, String content, LocalDateTime now);
 }
