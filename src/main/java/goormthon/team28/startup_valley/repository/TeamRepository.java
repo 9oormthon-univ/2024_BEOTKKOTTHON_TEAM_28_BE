fc71bb4fad7da4edab75c68e04be5c22a2570764
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-21 02:36:57 +0900

    Feat: 프로젝트 상태 변경 로직 추가

--- src/main/java/goormthon/team28/startup_valley/repository/TeamRepository.java
+++ src/main/java/goormthon/team28/startup_valley/repository/TeamRepository.java
@@ -2,6 +2,7 @@ package goormthon.team28.startup_valley.repository;
 
 import goormthon.team28.startup_valley.domain.Member;
 import goormthon.team28.startup_valley.domain.Team;
+import goormthon.team28.startup_valley.dto.type.EProjectStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +17,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     @Modifying(clearAutomatically = true)
     @Query("update Team t set t.leader = :leader where t.id = :teamId")
     void updateLeader(Long teamId, Member leader);
+    @Modifying(clearAutomatically = true)
+    @Query("update Team t set t.status = :status where t.id = :teamId")
+    void updateStatus(Long teamId, EProjectStatus status);
 }
