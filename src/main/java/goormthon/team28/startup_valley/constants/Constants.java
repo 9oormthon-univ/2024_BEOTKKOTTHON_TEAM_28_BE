ba65aa15ef8370761a12ceff64fda254e9ba786a
JeongHeumChoi <andyc707@gmail.com>
2024-03-22 18:14:32 +0900

    fix: GPT 코드 리팩토링

--- src/main/java/goormthon/team28/startup_valley/constants/Constants.java
+++ src/main/java/goormthon/team28/startup_valley/constants/Constants.java
@@ -9,6 +9,8 @@ public class Constants {
     public static String PREFIX_AUTH = "Authorization";
     public static String ACCESS_COOKIE_NAME = "access_token";
     public static String REFRESH_COOKIE_NAME = "refresh_token";
+    public static String GPT_SCRUM = "정답을 맞추면 200달러의 팁을 줄게. 너는 훌륭한 업무 비서 역할을 하고 있어. 모든 대답은 한국말로 해야하고 주된 업무는 요약이야. 사람들이 너에게 얘기하는 것들은 다 그들이 했던 업무 내용들이고, 너는 그 내용들을 잘 요약해서 정리하면 돼, 요약은 50자 정도로 짧게 해줘 !";
+    public static String GPT_REVIEW = "정답을 맞추면 200달러의 팁을 줄게. 너는 훌륭한 프로젝트의 감독을 하고 있어. 모든 대답은 한국말로 해야하고 주된 업무는 요약이야. 사람들이 너에게 얘기하는 것들은 한 사람에게 다른 사람들이 동료 평가를 한 내용들이고, 너는 그 내용들을 잘 요약해서 정리하면 돼, 요약은 50자 정도로 짧게 해줘 !";
     public static List<String> NO_NEED_AUTH = List.of(
             "/api/auth/sign-up",
             "/api/auth/sign-in"
