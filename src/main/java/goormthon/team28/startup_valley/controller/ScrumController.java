c69b64400ac0206c63f2dd15d6ea0c6c5acc3aa3
JeongHeumChoi <79458446+JeongHeumChoi@users.noreply.github.com>
2024-03-23 14:39:52 +0900

    🐛 [FIx/#31] - 스크럼 조회 API 수정 (#123)
    * chore: 크레덴셜 수정
    
    * fix: 업무시간 상세 수정 API 오류 해결
    
    * fix: 동료 평가 조회 API 수정
    
    * fix: R&R 작성 API 수정
    
    * fix: 스크럼 타임라인 API URI 수정 및 쿼리 파라미터 추가

--- src/main/java/goormthon/team28/startup_valley/controller/ScrumController.java
+++ src/main/java/goormthon/team28/startup_valley/controller/ScrumController.java
@@ -4,10 +4,7 @@ import goormthon.team28.startup_valley.annotation.UserId;
 import goormthon.team28.startup_valley.dto.global.ResponseDto;
 import goormthon.team28.startup_valley.service.ScrumService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +13,12 @@ public class ScrumController {
 
     private final ScrumService scrumService;
 
-    @GetMapping("/members/{membersId}/scrums")
-    public ResponseDto<?> listScrum(@UserId Long userId, @PathVariable Long membersId) {
-        return ResponseDto.ok(scrumService.listScrum(userId, membersId));
+    @GetMapping("/teams/{teamsId}/scrums")
+    public ResponseDto<?> listScrum(
+            @UserId Long userId,
+            @PathVariable Long teamsId,
+            @RequestParam(required = false) Long target
+    ) {
+        return ResponseDto.ok(scrumService.listScrum(userId, teamsId, target));
     }
 }
