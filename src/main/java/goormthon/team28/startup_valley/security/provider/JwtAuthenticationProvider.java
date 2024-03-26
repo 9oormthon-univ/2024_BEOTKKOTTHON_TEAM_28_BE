bd71e7867356765123940cb799a816a2bc926faa
Lim jeong woo <dlawjddn0105@gmail.com>
2024-03-26 11:54:41 +0900

    🚀 [Deploy] - 해커톤 이후, 수정사항 수정 후 배포 (#166)
    * refactor: 코드 스타일 수정 (#154)
    
    * Chore: 불필요한 부분 삭제 (#157)
    
    * 🐛 [Fix] - 작업 목록 API 수정 (#158)
    
    * refactor: 코드 스타일 수정
    
    * fix: Work 생성 날짜 기준으로 내림차순 설정
    
    * 🐛 [Fix] - PeerReview 단계 예외처리 (#160)
    
    * refactor: 코드 스타일 수정
    
    * fix: Work 생성 날짜 기준으로 내림차순 설정
    
    * fix: PeerReview일 경우 추가
    
    * Chore: discord command 이름 변경 (#163)
    
    * Hotfix: 비밀번호 인증 로직 추가 (#165)
    
    ---------
    
    Co-authored-by: JeongHeumChoi <79458446+JeongHeumChoi@users.noreply.github.com>

--- src/main/java/goormthon/team28/startup_valley/security/provider/JwtAuthenticationProvider.java
+++ src/main/java/goormthon/team28/startup_valley/security/provider/JwtAuthenticationProvider.java
@@ -9,12 +9,15 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationProvider implements AuthenticationProvider {
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final CustomUserDetailService customUserDetailService;
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -22,15 +25,20 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
         if (authentication.getPrincipal().getClass().equals(String.class)){
             // form login 요청인 경우
             log.info("로그인 로직 인증 과정");
-            return authOfBeforeLogin(authentication.getPrincipal().toString());
+            return authOfLogin(authentication);
 
         } else { // 로그인 이후의 경우에 String이 아닌 복합 정보가 들어오는 경우
             log.info("로그인한 사용자 검증 과정");
             return authOfAfterLogin((JwtUserInfo) authentication.getPrincipal());
         }
     }
-    private Authentication authOfBeforeLogin(String serialId){
-        UserPrincipal userPrincipal = customUserDetailService.loadUserByUsername(serialId);
+    private Authentication authOfLogin(Authentication authentication){
+        // DB에 저장된 실제 데이터
+        UserPrincipal userPrincipal = customUserDetailService.loadUserByUsername(authentication.getPrincipal().toString());
+
+        // 비밀번호 검증 로직
+        if (!bCryptPasswordEncoder.matches(authentication.getCredentials().toString(), userPrincipal.getPassword()))
+            throw new UsernameNotFoundException("비밀번호가 일치하지 않습니다 ! ");
         return new UsernamePasswordAuthenticationToken(userPrincipal, null, userPrincipal.getAuthorities());
     }
     private Authentication authOfAfterLogin(JwtUserInfo userInfo){
