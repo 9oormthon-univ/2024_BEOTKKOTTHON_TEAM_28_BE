9fb042ff8f29c6dabfda22d80e71985801c14428
JeongHeumChoi <79458446+JeongHeumChoi@users.noreply.github.com>
2024-05-03 18:23:18 +0900

    🚀 [Deploy] - 로그아웃 시 쿠키 삭제 반영 (#175)
    * refactor: 코드 스타일 수정 (#154)
    
    * Chore: 불필요한 부분 삭제 (#157)
    
    * 🐛 [Fix] - 작업 목록 API 수정 (#158)
    
    * refactor: 코드 스타일 수정
    
    * fix: Work 생성 날짜 기준으로 내림차순 설정
    
    * 🐛 [Fix] - PeerReview 단계 예외처리 (#160)
    
    * refactor: 코드 스타일 수정
    
    * fix: Work 생성 날짜 기준으로 내림차순 설정
    
    * fix: PeerReview일 경우 추가
    
    * Chore: discord command 이름 변경 (#163)
    
    * Hotfix: 비밀번호 인증 로직 추가 (#165)
    
    * 🖊️ [Chore] - README 수정 (#168)
    
    * refactor: 코드 스타일 수정
    
    * fix: Work 생성 날짜 기준으로 내림차순 설정
    
    * fix: PeerReview일 경우 추가
    
    * Chore: 프로젝트 readme 꾸미기
    
    * 🖊️ [Chore] - README 수정 (#169)
    
    * refactor: 코드 스타일 수정
    
    * fix: Work 생성 날짜 기준으로 내림차순 설정
    
    * fix: PeerReview일 경우 추가
    
    * Chore: 프로젝트 readme 꾸미기
    
    * chore: README 수정
    
    * 🐛 [Fix] - 백로그 타임라인 API (#171)
    
    * refactor: 코드 스타일 수정
    
    * fix: Work 생성 날짜 기준으로 내림차순 설정
    
    * fix: PeerReview일 경우 추가
    
    * Chore: 프로젝트 readme 꾸미기
    
    * chore: README 수정
    
    * fix: R&R 정렬 오류 해결
    
    * 🐛 [Fix] - 로그아웃 시 쿠키 삭제 (#174)
    
    * refactor: 코드 스타일 수정
    
    * fix: Work 생성 날짜 기준으로 내림차순 설정
    
    * fix: PeerReview일 경우 추가
    
    * Chore: 프로젝트 readme 꾸미기
    
    * chore: README 수정
    
    * Fix: 로그아웃 시, 쿠키 삭제 코드 수정
    
    ---------
    
    Co-authored-by: Lim jeong woo <dlawjddn0105@gmail.com>

--- src/main/java/goormthon/team28/startup_valley/util/CookieUtil.java
+++ src/main/java/goormthon/team28/startup_valley/util/CookieUtil.java
@@ -1,5 +1,6 @@
 package goormthon.team28.startup_valley.util;
 
+import goormthon.team28.startup_valley.constants.Constants;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,6 +18,29 @@ public class CookieUtil {
 
         response.addHeader("Set-Cookie", cookie.toString());
     }
+
+    public static void logoutCookie(HttpServletRequest request, HttpServletResponse response, String domain) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null)
+            return;
+
+        for (Cookie cookie : cookies) {
+            boolean isAccessCookie = cookie.getName().equals(Constants.ACCESS_COOKIE_NAME);
+            boolean isRefreshCookie = cookie.getName().equals(Constants.REFRESH_COOKIE_NAME);
+
+            if (isAccessCookie || isRefreshCookie) {
+                ResponseCookie tempCookie = ResponseCookie.from(cookie.getName(), cookie.getValue())
+                        .path("/")
+                        .domain(domain)
+                        .secure(true)
+                        .maxAge(0)
+                        .httpOnly(isRefreshCookie)
+                        .build();
+                response.addHeader("Set-Cookie", tempCookie.toString());
+            }
+        }
+    }
+
     public static void addSecureCookie(HttpServletResponse response, String domain, String key, String value, Integer maxAge){
         Cookie cookie = new Cookie(key, value);
         cookie.setPath("/");
@@ -26,6 +50,7 @@ public class CookieUtil {
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
     }
+
     public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name){
         Cookie[] cookies = request.getCookies();
         if (cookies == null){
