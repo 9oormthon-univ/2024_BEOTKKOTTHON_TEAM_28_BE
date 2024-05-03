eb82f5571859e013012a13ca9eae2eeede3ae1f9
JeongHeumChoi <andyc707@gmail.com>
2024-05-03 18:19:18 +0900

    Fix: 로그아웃 시, 쿠키 삭제 코드 수정

--- src/main/java/goormthon/team28/startup_valley/security/handler/logout/CustomLogoutResultHandler.java
+++ src/main/java/goormthon/team28/startup_valley/security/handler/logout/CustomLogoutResultHandler.java
@@ -8,6 +8,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -17,12 +18,17 @@ import java.io.IOException;
 @Slf4j
 @Component
 public class CustomLogoutResultHandler implements LogoutSuccessHandler {
+
+    @Value("${server.domain}")
+    private String domain;
+
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         if (authentication == null){
             log.info("인증 정보가 존재하지 않습니다. authentication is null..");
             AuthenticationResponse.makeFailureResponse(response, ErrorCode.NOT_FOUND_USER);
         }
+        CookieUtil.logoutCookie(request, response, domain);
         CookieUtil.deleteCookie(request, response, Constants.ACCESS_COOKIE_NAME);
         CookieUtil.deleteCookie(request, response, Constants.REFRESH_COOKIE_NAME);
         AuthenticationResponse.makeSuccessResponse(response);
